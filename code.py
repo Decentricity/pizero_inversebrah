be5cbbf2aaba1e9a3ffea918dee9927e7c56c46e
Pandu Sastrowardoyo <1@decentri.city>
2023-11-30 18:36:10 +0700

    Create code.py

---
+++ code.py
@@ -0,0 +1,63 @@
+
+import time
+import random
+import board
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+
+def expand_key(grammar, key):
+    if key not in grammar:
+        return key
+
+    phrase = random.choice(grammar[key])
+    return expand_phrase(grammar, phrase)
+
+def find_keys(phrase):
+    keys = []
+    key = ''
+    recording = False
+    
+    for char in phrase:
+        if char == '#' and not recording:
+            recording = True
+        elif char == '#' and recording:
+            keys.append(key)
+            key = ''
+            recording = False
+        elif recording:
+            key += char
+            
+    return keys
+
+def expand_phrase(grammar, phrase):
+    
+    while "#" in phrase:
+        keys = find_keys(phrase)
+        
+        for key in keys:
+            phrase = phrase.replace("#" + key + "#", expand_key(grammar, key), 1)
+            
+    return phrase
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)
+# inversebrah summon
+grammar = {
+    "origin": ["#inversebrah# #getmein# #lilshid# #aw#","#aw# #inversebrah# #getmein# #lilshid#"],
+    "inversebrah": ["@inversebrah", "", "inversebrah","inverted brother"],
+    "getmein": ["get me in", "put me in","me too", "come here", "come on in", "do your thing", "do your job", "screenshot dis", "yo","come in"],
+    "lilshid": ["smolting", "lil shid", "#snot# #salamander#",""],
+    "snot" : ["snot","ugly","green", "weird","hybrid","mutated"],
+    "salamander" : ["salamander","frok","platypus","cucumber","pickle"],
+    "aw" : ["aw","lmwo","lmeow","iwo","jfw" ""]
+}
+
+while True:
+    # Generate inversebrah notifs
+    smoltingsummon = expand_phrase(grammar, "#origin#")
+    # print(smoltingsummon)  # writeout get me in lil shid
+    keyboard_layout.write(smoltingsummon + "\n")
+    
+    # Let's not overwhelm the computer, shall we?
+    time.sleep(0.5)
